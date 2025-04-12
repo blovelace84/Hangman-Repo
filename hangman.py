fa9230d506193c37da0936ff9d81b1ea9baa3584
Shado <blovelace84@gmail.com>
2025-04-12 09:54:57 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,27 +1,23 @@
 import random
-import string
 
-import char
-
-#List of messages
 sentences = [
-    'Python is awesome',
-    "Hangman game in python",
-    "Sonic the Hedgehog is my favorite character",
-    "Super Monkey Ball",
-    "AI is the future. Be ready",
+    "Python is awesome",
+    "Hello world",
+    "OpenAI is the future. Be ready",
+    "Sonic the Hedgehog is my favorite game",
+    "Super Monkey Ball"
+    "Hangman game in Python",
     "Code like a pro"
 ]
 
-#Pick a random sentence
 sentence = random.choice(sentences).lower()
 
-#set up the game with _
+# Corrected list comprehension
 guessed_sentence = [char if not char.isalpha() else '_' for char in sentence]
 guessed_letters = set()
 tries = 6
 
-print("Welcome to Hangman. Ready to play?")
+print("🎮 Welcome to Sentence Hangman!")
 print("Message to guess: " + ' '.join(guessed_sentence))
 
 while tries > 0 and '_' in guessed_sentence:
@@ -38,21 +34,22 @@ while tries > 0 and '_' in guessed_sentence:
     guessed_letters.add(guess)
 
     if guess in sentence:
-        print("Correct!")
+        print("Good guess!")
         for i, char in enumerate(sentence):
             if char == guess:
                 guessed_sentence[i] = guess
     else:
-        print("Incorrect! Try again.")
+        print("Wrong guess.")
         tries -= 1
 
-    print("\nMessage:" + ' '.join(guessed_sentence))
+    print("\nMessage: " + ' '.join(guessed_sentence))
     print("Tries left:", tries)
     print("Guessed letters:", ', '.join(sorted(guessed_letters)))
+    print("-" * 40)
 
 if '_' not in guessed_sentence:
     print("Congratulations! You won!")
     print(f"\"{sentence}\"")
 else:
-    print("Sorry, you died! the message was:")
+    print("Game over! You died! The message was:")
     print(f"\"{sentence}\"")
